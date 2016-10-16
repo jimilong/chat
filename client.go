7ab6e5e91f758ed0c23876dd1ee1ed15216b7ac6
龙敏 <longmin@longmindeMacBook-Pro.local>
2016-10-16 11:04:07 +0800

    new verson

--- client.go
+++ client.go
@@ -1,34 +1,63 @@
 package main
 
 import (
-	"io"
-	"log"
+	"./protocal"
+	"bufio"
+	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
-//!+
+var quitSemaphore chan bool
+
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	//done := make(chan bool)
-	go func() {
-		//input and send
-		if _, err := io.Copy(conn, os.Stdin); err != nil {
-			log.Fatal(err)
-		}
-
-		//done <- true // signal the main goroutine
-	}()
-
-	//receive and show
-	io.Copy(os.Stdout, conn) // NOTE: ignoring errors
-	log.Println("done")
-	//<-done // wait for background goroutine to finish
+	//for i := 0; i < 5000; i++ {
+	go openConn()
+	//}
+	// b := []byte("time\n")
+	// conn.Write(b)
+	var msg string
+	fmt.Scanln(&msg)
+	<-quitSemaphore
 }
 
-//!-
+func openConn() {
+	var tcpAddr *net.TCPAddr
+	tcpAddr, _ = net.ResolveTCPAddr("tcp", "127.0.0.1:9999")
+
+	conn, _ := net.DialTCP("tcp", nil, tcpAddr)
+	defer conn.Close()
+	fmt.Println("connected!")
+
+	go onMessageRecived(conn)
+	go sendMessage(conn)
+	<-quitSemaphore
+}
+
+func sendMessage(conn *net.TCPConn) {
+	for {
+		time.Sleep(1 * time.Second)
+		// var msg string
+		// fmt.Scanln(&msg)
+		// fmt.Println(msg)
+		var msg []byte
+		n, _ := os.Stdin.Read(msg)
+
+		b, _ := protocal.Pack(string(msg[:n]))
+
+		conn.Write(b)
+	}
+}
+
+func onMessageRecived(conn *net.TCPConn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := protocal.Unpack(reader)
+		fmt.Println(msg)
+		if err != nil {
+			quitSemaphore <- true
+			break
+		}
+	}
+}
--- server.go
+++ server.go
@@ -1,138 +1,59 @@
 package main
 
 import (
+	"./protocal"
 	"bufio"
 	"fmt"
-	"log"
 	"net"
-	"time"
 )
 
-type client chan string
-
-var (
-	entering = make(chan client)
-	leaving  = make(chan client)
-	message  = make(chan string)
-)
-
-//msg transmit center
-func broadcaster() {
-	clients := make(map[client]bool)
-
-	for {
-		select {
-		case msg := <-message:
-			for cli := range clients {
-				cli <- msg
-			}
-		case cli := <-entering:
-			clients[cli] = true
-		case cli := <-leaving:
-			delete(clients, cli)
-			close(cli)
-		}
-	}
-}
-
-func clientWriter(conn net.Conn, ch chan string) {
-	for msg := range ch {
-		fmt.Fprintln(conn, msg)
-	}
-}
-
-func clientInput(conn net.Conn, out chan bool, in chan bool) {
-	input := bufio.NewScanner(conn)
-	who := conn.RemoteAddr().String()
-	for input.Scan() {
-		message <- who + ": " + input.Text()
-		in <- true
-	}
-
-	out <- true
-}
-
-func handleTimeout(timeout chan bool, in chan bool) {
-	tick := time.Tick(30 * time.Second)
-	msgs := make([]bool, 0, 10)
-	flag := false
-	for {
-		select {
-		case msg := <-in:
-			if len(msgs) < 1 {
-				msgs = append(msgs, msg)
-			}
-
-			fmt.Println(msgs)
-		case <-tick:
-			if len(msgs) == 0 {
-				fmt.Println("time-out")
-				timeout <- true
-				flag = true
-			} else {
-				msgs = msgs[:0]
-			}
-		}
-		if flag {
-			break
-		}
-	}
-	fmt.Println("handleTimeout over 88")
-}
-
-func handleConn(conn net.Conn) {
-	ch := make(chan string)
-	in := make(chan bool, 1)
-	out := make(chan bool)
-	timeout := make(chan bool)
-
-	//send msg to client
-	go clientWriter(conn, ch)
-
-	who := conn.RemoteAddr().String()
-
-	ch <- "You are " + who
-	message <- who + "has arrived"
-	entering <- ch
-
-	//超时处理
-	go handleTimeout(timeout, in)
-
-	//receive msg from client
-	go clientInput(conn, out, in)
-
-	for {
-		select {
-		case <-out:
-			leaving <- ch
-			message <- who + ": have left"
-			conn.Close()
-		case <-timeout:
-			leaving <- ch
-			message <- who + ": lose contact"
-			conn.Close()
-		}
-		break
-	}
-
-	fmt.Println(who + " client over")
-}
+var ConnMap map[string]*net.TCPConn
 
 func main() {
-	listner, err := net.Listen("tcp", "127.0.0.1:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
+	var tcpAddr *net.TCPAddr
+	ConnMap = make(map[string]*net.TCPConn)
+	tcpAddr, _ = net.ResolveTCPAddr("tcp", "127.0.0.1:9999")
 
-	go broadcaster()
+	tcpListener, _ := net.ListenTCP("tcp", tcpAddr)
+
+	defer tcpListener.Close()
 
 	for {
-		conn, err := listner.Accept()
+		tcpConn, err := tcpListener.AcceptTCP()
 		if err != nil {
-			log.Print(err)
 			continue
 		}
 
-		go handleConn(conn)
+		fmt.Println("A client connected : " + tcpConn.RemoteAddr().String())
+		ConnMap[tcpConn.RemoteAddr().String()] = tcpConn
+		go handleConn(tcpConn)
+	}
+
+}
+
+func handleConn(conn *net.TCPConn) {
+	ipStr := conn.RemoteAddr().String()
+	defer func() {
+		fmt.Println("disconnected :" + ipStr)
+		conn.Close()
+	}()
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := protocal.Unpack(reader)
+		if err != nil {
+			return
+		}
+		fmt.Println(conn.RemoteAddr().String() + ":" + string(message))
+
+		b, err := protocal.Pack(conn.RemoteAddr().String() + ":" + string(message))
+		if err != nil {
+			continue
+		}
+		//boradcastMessage
+		for _, conn := range ConnMap {
+			conn.Write(b)
+		}
+
 	}
 }
